ec35307e324844343ecda7f1bf008d0848d98a07
Zirconova <zirconova@gmail.com>
2024-08-27 23:54:24 -0600

    redo tilemaps with pytmx

--- code/level.py
+++ code/level.py
@@ -2,8 +2,8 @@ import pygame
 from settings import *
 from player import Player
 from camera import Camera
-from tileset import Tileset
-from tilemap import Tilemap
+from tile import Tile
+from pytmx.util_pygame import load_pygame
 
 class Level:
     def __init__(self):
@@ -17,11 +17,15 @@ class Level:
         # camera
         self.camera = Camera(SCREEN_WIDTH, SCREEN_HEIGHT)
 
-        self.tileset = Tileset('graphics/Sprites/Objects&Tiles/tilemap_1.png', (16, 16))
-        self.tilemap = Tilemap(self.tileset, self.all_sprites, (1, 4))
-        self.tilemap.set([[3,5,1,6]])
+        # self.tmx_data = load_pygame("graphics/Sprites/Objects&Tiles/tilemap_1.tmx")
+        # for layer in self.tmx_data.visible_layers:
+        #     if hasattr(layer, "data"):
+        #         for x, y, surf in layer.tiles():
+        #             pos = (x * 32, y * 32)
+        #             Tile(pos, surf, self.all_sprites)
 
         self.player = Player((640, 360), self.all_sprites)
+
     def run(self, dt):
         self.display_surface.fill('white')
         # manually draw all sprites with camera adjustment
---
+++ code/tile.py
@@ -0,0 +1,7 @@
+import pygame
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, pos, surf, group):
+        super().__init__(group)
+        self.image = surf
+        self.rect = self.image.get_rect(topleft=pos)
