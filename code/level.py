65f4962877e706e401317333b5c62da049f7acc2
bikitoria <victoriapiquer.3@gmail.com>
2024-08-27 22:50:08 -0600

    forgot what i did

--- code/level.py
+++ code/level.py
@@ -2,22 +2,26 @@ import pygame
 from settings import *
 from player import Player
 from camera import Camera
+from tileset import Tileset
+from tilemap import Tilemap
 
 class Level:
     def __init__(self):
         # get the display surface
+
         self.display_surface = pygame.display.get_surface()
 
         # sprite groups
         self.all_sprites = pygame.sprite.Group()
 
+        # camera
         self.camera = Camera(SCREEN_WIDTH, SCREEN_HEIGHT)
 
-        self.setup()
+        self.tileset = Tileset('graphics/Sprites/Objects&Tiles/tilemap_1.png', (16, 16))
+        self.tilemap = Tilemap(self.tileset, self.all_sprites, (1, 4))
+        self.tilemap.set([[3,5,1,6]])
 
-    def setup(self):
         self.player = Player((640, 360), self.all_sprites)
-
     def run(self, dt):
         self.display_surface.fill('white')
         # manually draw all sprites with camera adjustment
--- code/player.py
+++ code/player.py
@@ -30,7 +30,6 @@ class Player(pygame.sprite.Sprite):
         self.frame_index += 10 * dt
         if self.frame_index >= len(self.animations[self.status]):
             self.frame_index = 0
-        print(len(self.animations[self.status]))
         self.image = self.animations[self.status][int(self.frame_index)]
 
     def import_assets(self):
@@ -105,19 +104,19 @@ class Player(pygame.sprite.Sprite):
 
     def input(self):
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_UP]:
+        if keys[pygame.K_w]:
             self.direction.y = -1
             self.status = 'up'
-        elif keys[pygame.K_DOWN]:
+        elif keys[pygame.K_s]:
             self.direction.y = 1
             self.status = 'down'
         else:
             self.direction.y = 0
 
-        if keys[pygame.K_RIGHT]:
+        if keys[pygame.K_d]:
             self.direction.x = 1
             self.status = 'right'
-        elif keys[pygame.K_LEFT]:
+        elif keys[pygame.K_a]:
             self.direction.x = -1
             self.status = 'left'
         else:
